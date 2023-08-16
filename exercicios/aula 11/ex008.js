fb375179ede2276b8aff8e96492fe7fb3caad4c7
THIAGO FRANÇA <135141078+ThiagoFrancca@users.noreply.github.com>
2023-08-16 19:02:43 -0300

    condição

---
+++ exercicios/aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.8
+console.log(`A velocidade do carro é ${vel}km/h`)
+if(vel > 50){
+    console.log('Você ultrapassou a velocidade de 50km/h. MULTADO')
+    
+}
+console.log('Dirija com o sinto de segurança')
---
+++ exercicios/aula 11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Brasil'
+console.log(`Nascendo em: ${país}`)
+if( país == 'Brasil'){
+    console.log('Brasileiro mané')
+}
+else{
+    console.log('estrangeiro')
+}
