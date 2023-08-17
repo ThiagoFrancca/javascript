e64dd9d3b9d3421372a20a53ce8d0d800aea7368
THIAGO FRANÇA <135141078+ThiagoFrancca@users.noreply.github.com>
2023-08-17 19:47:00 -0300

    ex de condições

---
+++ exercicios/aula 12/ex0011.js
@@ -0,0 +1,11 @@
+var idade =62
+if (idade <16){
+    console.log('Não pode votar!')
+}else if (idade < 18){
+    console.log('Voto facultativo')
+}else if(idade < 65){
+    console.log(' Voto Obrigatório')
+}
+else{
+    console.log('Voto facultativo')
+}
