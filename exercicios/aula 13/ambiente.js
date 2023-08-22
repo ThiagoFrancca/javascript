4e2fe74b3473aeba8ab7d55666cfe154d674cd3b
THIAGO FRANÇA <135141078+ThiagoFrancca@users.noreply.github.com>
2023-08-22 16:05:20 -0300

    aulas 13,14

---
+++ exercicios/aula 13/ambiente.js
@@ -0,0 +1,22 @@
+var pizza = 1
+
+do{console.log('Come')
+pizza++
+}while(pizza <= 8)
+
+
+
+/*while(pizza <= 8){
+    console.log('Come arromabado')
+    pizza++
+}
+
+/*console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+console.log('Olá, mundo!')
+*/
