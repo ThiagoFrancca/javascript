b017af5a2fea8e9cc05da56d469d4a56eb74edc4
THIAGO FRANÇA <135141078+ThiagoFrancca@users.noreply.github.com>
2023-08-26 14:38:46 -0300

    .

---
+++ tabuada/ex017.js
@@ -0,0 +1,17 @@
+function multiplicar(){
+    var num = document.getElementById('Fnum')
+    var resposta = document.getElementById('resposta')
+
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+
+    }
+
+
+
+
+
+
+
+
+}
