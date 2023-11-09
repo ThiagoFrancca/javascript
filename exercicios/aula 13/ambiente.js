cc45ad7c48674de922eab6da3e25312ae7cbd9c1
THIAGO FRANÇA <135141078+ThiagoFrancca@users.noreply.github.com>
2023-11-09 19:25:03 -0300

    Update ambiente.js

--- exercicios/aula 13/ambiente.js
+++ exercicios/aula 13/ambiente.js
@@ -1,22 +1,24 @@
-var pizza = 1
+var pizza= 1
 
-do{console.log('Come')
-pizza++
-}while(pizza <= 8)
-
-
-
-/*while(pizza <= 8){
-    console.log('Come arromabado')
+do{
+    console.log(`comer, fatia ${pizza}`)
     pizza++
-}
+}while( pizza <= 8)
 
-/*console.log('Olá, mundo!')
-console.log('Olá, mundo!')
-console.log('Olá, mundo!')
-console.log('Olá, mundo!')
-console.log('Olá, mundo!')
-console.log('Olá, mundo!')
-console.log('Olá, mundo!')
-console.log('Olá, mundo!')
-*/
+
+
+/*while (pizza <= 8){
+    console.log (`fatia ${pizza}`)
+    pizza++ // pizza= pizza +1
+}*/
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
