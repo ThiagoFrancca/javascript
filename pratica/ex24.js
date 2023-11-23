d856823060a1cb84ef9fa4178bbe877c7d22f132
THIAGO FRANÇA <135141078+ThiagoFrancca@users.noreply.github.com>
2023-11-23 12:11:39 -0300

    c

---
+++ pratica/ex24.js
@@ -0,0 +1,38 @@
+
+console.log('INICIO') 
+   
+
+for(let c= 1; c<= 100;  c++){
+if ( c%3===0 && c%5===0){
+    console.log(c,"FIZZBUZZ")
+}else if( c%3===0){
+    console.log(c,"FIZZ")
+}else if( c%5===0){
+    console.log(c,"BUZZ")
+}else{
+    console.log(c)
+}
+
+
+   
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+ 
+
---
+++ pratica/ex25.js
@@ -0,0 +1,9 @@
+function regresiva(n){
+    
+    for( let num=1; num<=n; n--){
+        console.log(n)
+
+    }
+}
+let res= regresiva(10)
+console.log(res)
